4b462dd2ccc40395d61df0dc7e33d525e9a90b81
kumanan <kumananlakshmanasamy@gmail.com>
2025-04-18 21:55:39 +0530

    auth

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,14 +1,51 @@
+import auth from '../model/authModule.js'
+import jwt from 'jsonwebtoken'
 export const createAccount = async (req, res) => {
-// usename 
-// email id 
-// password
-res.json('Account Created')
+const {email, password, name } = req.body
+
+try {
+    // check if user already exists
+    const userExist = await auth.findOne({email})
+    if (userExist) {
+        throw new Error("user already exists")
+    }
+    const user = await auth.create({email,password,name})
+    const token = jwt.sign({id:user._id},'demo', {
+        expiresIn: '30d'
+    })
+    return res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token
+    })
+} catch (error) {
+    return res.status(400).json({message:error.message})
+}
 }
 
 export const login = async (req, res) => {
-// email
-// password
-res.json('login successfully')
+    const {email, password } = req.body
+    try {
+        const userExist = await auth.findOne({email})
+        if (!userExist) {
+            throw new Error("user not found")
+        }
+        if (!await userExist.matchPassword(password)) {
+            throw new Error("password not matched")
+        }
+        const token = jwt.sign({id:userExist._id},'demo', {
+            expiresIn: '30d'
+        })
+        res.status(200).json({
+            _id: userExist._id,
+            name: userExist.name,
+            email: userExist.email,
+            token
+        })
+    } catch (error) {
+        return res.status(400).json({message:error.message})
+    }
 }
 
 export const logout = async (req, res) => {
--- backend/model/authModule.js
+++ backend/model/authModule.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 const authSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -18,4 +19,17 @@ const authSchema = new mongoose.Schema({
         default: "user"
     }
 })
+
+authSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password,salt)
+    }
+    next()
+})
+
+authSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
 export default mongoose.model('auth', authSchema)
